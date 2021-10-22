a83c23d0d69492f86e9cbd08220ba8cb46dc0ea0
BalasafAlizade <balasef2601@gmail.com>
2021-10-22 18:36:01 +0400

    evde baxim

---
+++ src/Artiqq.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Artiqq {
+    public static void main(String[] args) {
+        System.out.print("eded=>");
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+        for (int i = 0; i <=a ; i++) {
+            for (int j = 1; j -1<i ; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
--- src/Classik.java
+++ src/Classik.java
@@ -4,16 +4,32 @@ public class Classik {
 
     public static void main(String[] args) {
 
-        System.out.print("sira");
+        System.out.print("sira=>");
         Scanner sc = new Scanner(System.in);
         int sira = sc.nextInt();
         int say = 1;
         for (int i = 1; i <= sira; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(say);
+                System.out.print(say+" ");
                 say++;
             }
             System.out.println();
+
+        }
+
+
+        System.out.print("piramida ededi  =>");
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            for (int i = 1; i <= a; i++){
+
+                for (int j = a; j > i; j--){
+                    System.out.print(" ");
+                }
+                for (int k = 0; k < i; k++) {
+                    System.out.print(i + " ");
+                }
+                System.out.println(" ");
+            }
         }
     }
-}
